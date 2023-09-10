b9f2b8392eca8eafbe2a522a638bb104dfe8c569
Kevin Galligan <kgalligan@gmail.com>
2023-09-09 22:57:46 -0400

    Basic method

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { parse } from 'dot-properties'
+import * as fs from 'fs'
 
 /**
  * The main function for the action.
@@ -7,18 +8,19 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const file: string = core.getInput('file')
+    const property: string = core.getInput('property')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`read-property: file-${file}`)
+    core.debug(`read-property: property-${property}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const src = fs.readFileSync(file, 'utf8')
+    const obj = parse(src)
+    const propVal = obj[property]
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.debug(`read-property: propVal-${propVal}`)
+
+    core.setOutput(property, propVal)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
